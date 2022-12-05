6ee21bc8280d715bc8bed381115f9d8a1bf1a108
brunnohs <119610539+brunnohs@users.noreply.github.com>
2022-12-05 14:26:56 -0300

    Update model.py

--- model.py
+++ model.py
@@ -9,19 +9,19 @@ Versão: 0.0.3
 import os
 import shutil
 
-def arquivos:
+def arquivos():
+    
+    os.chdir(str(pasta_python))
 
-os.chdir(str(pasta_python))
+    with open("DespesasCorrentes.xlsx","w") as arquivo_1:
+        arquivo_1.write("Despesas Correntes")
+    with open("DespesasCorrentes.docx","w") as arquivo_2:
+        arquivo_2.write("Despesas Correntes")
+    with open("DespesasCapital.xlsx","w") as arquivo_3:
+        arquivo_3.write("Despesas Capital")
+    with open("DespesasCapital.docx","w") as arquivo_4:
+        arquivo_4.write("Despesas Capital")
+    with open("Relatorio.docx","w") as arquivo_5:
+        arquivo_5.write("Relatorio")  
 
-with open("DespesasCorrentes.xlsx","w") as arquivo_1:
-    arquivo_1.write("Despesas Correntes")
-with open("DespesasCorrentes.docx","w") as arquivo_2:
-    arquivo_2.write("Despesas Correntes")
-with open("DespesasCapital.xlsx","w") as arquivo_3:
-    arquivo_3.write("Despesas Capital")
-with open("DespesasCapital.docx","w") as arquivo_4:
-    arquivo_4.write("Despesas Capital")
-with open("Relatorio.docx","w") as arquivo_5:
-    arquivo_5.write("Relatorio")  
-
-return files
+    return files
